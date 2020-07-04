8043d264e7ac6edb6fde2f61f9a6a38ab34eac24
bchae <cbr5964@naver.com>
2020-07-04 14:16:30 +0900

    7/4

---
+++ 2579.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+using namespace std;
+
+int* D;
+int* C;
+int N;
+
+int Method(int index) {
+
+	int d1 = -1;
+	int d2 = -1;
+
+	d1 = D[index - 2] + C[index];
+	d2 = D[index - 3] + C[index - 1] + C[index];
+
+	if (d1 >= d2) {return d1; }
+	if (d2 >= d1) {return d2; }
+}
+
+int main()
+{
+	cin >> N;
+
+	D = (int*)malloc(sizeof(int) * (N + 1));
+	C = (int*)malloc(sizeof(int) * (N + 1));
+
+	int i;
+	for (i = 1; i <= N; i++) {
+		cin >> C[i];
+	}
+
+	D[0] = 0; D[1] = C[1]; C[0] = 0;
+
+	if (N >= 2) {
+		D[2] = C[1] + C[2];
+	}
+	if (N >= 3) {
+		if (C[1] > C[2]) {
+			D[3] = C[1] + C[3];
+		}
+		else {
+			D[3] = C[2] + C[3];
+		}
+	}
+
+	for (i = 4; i <= N; i++) {
+		D[i] = Method(i);
+	}
+
+	cout << D[N];
+}
---
+++ 빨간공파랑공.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+using namespace std;
+
+int* D;
+int* C;
+int* x;
+int N;
+
+int Method(int index) {
+	
+	int d1 = -1;
+	int d2 = -1;
+	int d3 = -1;
+
+	d1 = D[index - 2] + C[index];
+	if (index >= 3) {
+		d2 = D[index - 3] + D[index - 1] + C[index];
+	}
+	if (index >= 4) {
+		d3 = D[index - 4] + D[index - 2] + D[index - 1] + C[index];
+	}
+
+	if (d1 > d2 && d1 > d3) return d1;
+	if (d2 > d1 && d2 > d3) return d2;
+	if (d3 > d1 && d3 > d2) return d3;
+}
+
+int main()
+{
+	cin >> N;
+
+	cout << N;
+
+	D = (int*)malloc(sizeof(int) * (N+1));
+	C = (int*)malloc(sizeof(int) * (N+1));
+	x = (int*)malloc(sizeof(int) * (N+1));
+
+	int i;
+	for (i = 1; i <= N; i++) {
+		cin >> C[i];
+	}
+
+	cout << N;
+
+	D[0] = 0; D[1] = C[1]; x[0] = 0; x[1] = 1;
+
+	for (i = 2; i <= N; i++) {
+		C[i] = Method(i);
+	}
+
+	cout << C[N];
+}
