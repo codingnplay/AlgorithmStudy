6066c9e64869ccefb51a68d58a775ae4fea70d41
b-chae <54340317+b-chae@users.noreply.github.com>
2020-07-20 20:42:23 +0900

    DFS) 인구 이동
    2번 시도 끝에 품

--- 16234.cpp
+++ 16234.cpp
@@ -1,174 +1,91 @@
-// 아직 미완성
-
 #include <iostream>
-#include <list>
-#include <vector>
-
 using namespace std;
 
-int** board;
-bool** visited;
-bool** visited2;
 int N;
+int board[50][50];
+bool rightOpen[50][50];
+bool downOpen[50][50];
+bool visited[50][50];
+bool visited2[50][50];
+int people;
 int L, R;
-int MoveCount = 0;
 
-int subtract(int x, int y) {
-	if (x > y) return x - y;
-	else return y - x;
-}
-
-bool checkLR(int x, int y) {
-	if (subtract(x, y) >= L && subtract(x, y) <= R) return true;
-	return false;
-}
-
-bool boundaryOpen() {
-	int i, j;
-	bool returnValue = false;
-
-	for (i = 0; i < 2*N + 1; i++) {
-		for (j = 0; j < 2*N + 1; j++) {
-			visited[i][j] = true;
-		}
-	}
-
-	for (i = 0; i < N; i++) {
-		for (j = 0; j < N; j++) {
-			if (i!=N-1 && checkLR(board[i + 1][j], board[i][j])) {
-				visited[2*i][2*j] = false;
-				visited[2 * i + 1][2 * j] = false;
-				visited[2*(i + 1)][2*j] = false;
-				returnValue = true;
-			}
-			if (j != N-1 && checkLR(board[i][j + 1], board[i][j])) {
-				visited[2*i][2*j] = false;
-				visited[2 * i][2 * j + 1] = false;
-				visited[2*i][2*(j + 1)] = false;
-				returnValue = true;
-			}
-		}
-	}
-
-	return returnValue;
-}
-
-int dfs_how_many_country(int startI, int startJ, bool** visited) {
-	int country = 1;
-
-	visited[2*startI][2*startJ] = true;
-
-	if (!visited[2 * startI + 1][2 * startJ]) {
-		visited[2 * startI + 1][2 * startJ] = true;
-		country += dfs_how_many_country(startI + 1, startJ, visited);
-	}
-	if (!visited[2 * startI][2 * startJ + 1]) {
-		visited[2 * startI][2 * startJ + 1] = true;
-		country += dfs_how_many_country(startI, startJ + 1, visited);
-	}
-	if (startI != 0 && !visited[2 * startI - 1][2 * startJ]) {
-		visited[2 * startI - 1][2 * startJ] = true;;
-		country += dfs_how_many_country(startI - 1, startJ, visited);
-	}
-	if (startJ != 0 && !visited[2 * startI][2 * startJ - 1]) {
-		visited[2 * startI][2 * startJ - 1] = true;
-		country += dfs_how_many_country(startI, startJ - 1, visited);
-	}	
-
-	return country;
-}
-
-int dfs_return_pop(int startI, int startJ, bool** visited) {
-
-	int pop = board[startI][startJ];
-	visited[2 * startI][2 * startJ] = true;
-
-	if (!visited[2 * startI + 1][2 * startJ]) {
-		visited[2 * startI + 1][2 * startJ] = true;
-		pop += dfs_return_pop(startI + 1, startJ, visited);
-	}
-	if (!visited[2 * startI][2 * startJ + 1]) {
-		visited[2 * startI][2 * startJ + 1] = true;
-		pop += dfs_return_pop(startI, startJ + 1, visited);
-	}
-	if (startI != 0 && !visited[2 * startI - 1][2 * startJ]) {
-		visited[2 * startI - 1][2 * startJ] = true;;
-		pop += dfs_return_pop(startI - 1, startJ, visited);
-	}
-	if (startJ != 0 && !visited[2 * startI][2 * startJ - 1]) {
-		visited[2 * startI][2 * startJ - 1] = true;
-		pop += dfs_return_pop(startI, startJ - 1, visited);
-	}
-
-	return pop;
-}
-
-void print_board();
-
-void dfs_change_pop(int startI, int startJ, bool** visited, int value) {
-
-	visited[2 * startI][2 * startJ] = true;
-	board[startI][startJ] = value;
-
-	if (!visited[2 * startI + 1][2 * startJ]) {
-		visited[2 * startI + 1][2 * startJ] = true;
-		dfs_change_pop(startI + 1, startJ, visited, value);
-	}
-	if (!visited[2 * startI][2 * startJ + 1]) {
-		visited[2 * startI][2 * startJ + 1] = true;
-		dfs_change_pop(startI, startJ + 1, visited, value);
-	}
-	if (startI != 0 && !visited[2 * startI - 1][2 * startJ]) {
-		visited[2 * startI - 1][2 * startJ] = true;;
-		dfs_change_pop(startI - 1, startJ, visited, value);
-	}
-	if (startJ != 0 && !visited[2 * startI][2 * startJ - 1]) {
-		visited[2 * startI][2 * startJ - 1] = true;
-		dfs_change_pop(startI, startJ - 1, visited, value);
-	}
-
-}
-
-void cpy(bool** visited, bool** visited2) {
-	for (int i = 0; i < 2*N + 1; i++) {
-		for (int j = 0; j < 2*N + 1; j++) {
-			visited2[i][j] = visited[i][j];
-		}
-	}
-}
-
-void print_board() {
+void resetVisited() {
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
-			cout << board[i][j] << "\t";
+			visited[i][j] = false;
+			visited2[i][j] = false;
 		}
-		cout << "\n";
 	}
-	cout << "\n";
 }
 
-void print_boundary() {
-	for (int i = 0; i < 2*N + 1; i++) {
-		for (int j = 0; j < 2*N + 1; j++) {
-			cout << visited[i][j] << "\t";
+bool check() {
+	bool flag = false;
+	
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			rightOpen[i][j] = false;
+			downOpen[i][j] = false;
 		}
-		cout << "\n";
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (i < N - 1 && abs(board[i][j] - board[i + 1][j]) >= L && abs(board[i][j] - board[i + 1][j]) <= R) {
+				downOpen[i][j] = true;
+				flag = true;
+			}
+			if (j < N - 1 && abs(board[i][j] - board[i][j + 1]) >= L && abs(board[i][j] - board[i][j + 1]) <= R) {
+				rightOpen[i][j] = true;
+				flag = true;
+			}
+		}
+	}
+
+	return flag;
+}
+
+int dfs_country(int i, int j) {
+	visited[i][j] = true;
+
+	int count = 1;
+	people += board[i][j];
+	if (i > 0 && !visited[i - 1][j] && downOpen[i - 1][j]) {
+		count += dfs_country(i - 1, j);
+	}
+	if (j > 0 && !visited[i][j - 1] && rightOpen[i][j - 1]) {
+		count += dfs_country(i, j - 1);
+	}
+	if (i < N - 1 && !visited[i + 1][j] && downOpen[i][j]) {
+		count += dfs_country(i + 1, j);
+	}
+	if (j < N - 1 && !visited[i][j + 1] && rightOpen[i][j]) {
+		count += dfs_country(i, j + 1);
+	}
+	return count;
+}
+
+void dfs_change_ppl(int i, int j, int ppl) {
+	visited2[i][j] = true;
+	board[i][j] = ppl;
+	if (i > 0 && !visited2[i - 1][j] && downOpen[i - 1][j]) {
+		dfs_change_ppl(i - 1, j, ppl);
+	}
+	if (j > 0 && !visited2[i][j - 1] && rightOpen[i][j - 1]) {
+		dfs_change_ppl(i, j - 1, ppl);
+	}
+	if (i < N - 1 && !visited2[i + 1][j] && downOpen[i][j]) {
+		dfs_change_ppl(i + 1, j, ppl);
+	}
+	if (j < N - 1 && !visited2[i][j + 1] && rightOpen[i][j]) {
+		dfs_change_ppl(i, j + 1, ppl);
 	}
 }
 
 int main()
 {
-	int i, j;
-
 	cin >> N >> L >> R;
-
-	board = (int**)malloc(sizeof(int*) * (N));
-	visited = (bool**)malloc(sizeof(bool*) * (2*N+ 1));
-	visited2 = (bool**)malloc(sizeof(bool*) * (2*N + 1));
-
-	for (i = 0; i < N; i++) {
-		board[i] = (int*)malloc(sizeof(int) * (N));
-	}
+	int i, j;
 
 	for (i = 0; i < N; i++) {
 		for (j = 0; j < N; j++) {
@@ -176,27 +93,26 @@ int main()
 		}
 	}
 
-	for (i = 0; i < 2*N + 1; i++) {
-		visited[i] = (bool*)malloc(sizeof(bool) * (2*N + 1));
-		visited2[i] = (bool*)malloc(sizeof(bool) * (2*N + 1));
-	}
+	int ans = 0;
 
-	while (boundaryOpen()) {
-		print_boundary();
-		MoveCount++;
+	while (check()) {
+		resetVisited();
 		for (i = 0; i < N; i++) {
 			for (j = 0; j < N; j++) {
-				if (!visited[2*i][2*j]) {
-					cpy(visited, visited2);
-					int Countries = dfs_how_many_country(i, j, visited2);
-					cpy(visited, visited2);
-					int Pop = dfs_return_pop(i, j, visited2);
-					dfs_change_pop(i, j, visited, Pop / Countries);
-					print_board();
-					cout << Countries << " " << Pop << "\n";
+				if (!visited[i][j]) {
+					people = 0;
+					int country = dfs_country(i, j);
+					if (country >= 2) {
+						dfs_change_ppl(i, j, people / country);
+					}
 				}
 			}
 		}
+		ans += 1;
+		if (ans >= 2000) break;
 	}
-	cout << MoveCount << "\n";
+
+	cout << ans;
+
+	return 0;
 }
