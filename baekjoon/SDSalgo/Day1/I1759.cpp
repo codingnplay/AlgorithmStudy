6b06b2ed82cff1f57930616355ba03156b8ceb6a
b-chae <54340317+b-chae@users.noreply.github.com>
2020-08-10 12:03:13 +0900

    브루트포스) 암호 만들기

---
+++ baekjoon/SDSalgo/Day1/I1759.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+//a, n, t, c, i
+
+int L, C;
+bool visited[26] = { false, };
+char A[26];
+int selected = 0;
+
+void dfs(int idx) {
+	visited[A[idx] - 'a'] = true;
+	selected++;
+
+	int i;
+	if (selected == L) {
+		int vowel = 0, consonant = 0;
+		for (i = 0; i < 26; i++) {
+			if (visited[i]) {
+				if ('a' + i == 'a' || 'a' + i == 'e' || 'a' + i == 'i' || 'a' + i == 'o' || 'a' + i == 'u') {
+					vowel++;
+				}
+				else {
+					consonant++;
+				}
+			}
+		}
+		if (vowel >= 1 && consonant >= 2) {
+			for (i = 0; i < 26; i++) {
+				if (visited[i]) {
+					printf("%c", 'a' + i);
+				}
+			}
+			cout << "\n";
+		}
+	}
+	else {
+		for (i = idx + 1; i < C; i++) {
+			if (!visited[A[i] - 'a']) {
+				dfs(i);
+			}
+		}
+	}
+
+	visited[A[idx] - 'a'] = false;
+	selected--;
+}
+
+int main()
+{
+	cin >> L >> C;
+	int i;
+	for (i = 0; i < C; i++) {
+		cin >> A[i];
+	}
+
+	sort(A, A + C);
+
+	for (i = 0; i < C; i++) {
+		dfs(i);
+	}
+	return 0;
+}
