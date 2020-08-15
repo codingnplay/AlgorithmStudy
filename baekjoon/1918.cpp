890de69c1efd2f3b4ddcf2880882fbd2ccff287f
b-chae <54340317+b-chae@users.noreply.github.com>
2020-08-15 16:25:36 +0900

    스택 ) 후위 표기식

--- baekjoon/1918.cpp
+++ baekjoon/1918.cpp
@@ -1,67 +1,46 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
+string str;
 vector<char> stack;
 
 int main()
 {
-	string str;
-	cin >> str;
-	int i; char c;
-	for (i = 0; i < str.size(); i++) {
-		if (str[i] >= 'A' && str[i] <= 'Z') cout << str[i];
-		else if (str[i] == ')') {
-			while (!stack.empty()) {
-				c = stack.back();
-				stack.pop_back();
-				if (c != '(') cout << c;
-				else {
-					break;
-				}
-			}
-		}
-		else if (str[i] == '+' || str[i] == '|') {
-			if(!stack.empty()){
-				if (stack.back() == '*' || stack.back() == '/') {
-					while (!stack.empty()) {
-						c = stack.back();
-						if (c != '+' && c != '-') {
-							stack.pop_back();
-							if (c != '(') cout << c;
-						}
-						else {
-							break;
-						}
-					}
-				}
-			}
-			stack.push_back(str[i]);
-		}
-		else {
-			if (!stack.empty()) {
-				if (stack.back() == '*' || stack.back() == '/') {
-					while (!stack.empty()) {
-						c = stack.back();
-						if (c != '+' && c != '-') {
-							stack.pop_back();
-							if (c != '(') cout << c;
-						}
-						else {
-							break;
-						}
-					}
-				}
-			}
-			stack.push_back(str[i]);
-		}
-	}
+    cin >> str;
+    int i;
+    for (i = 0; i < str.length(); i++) {
+        if (str[i] >= 'A' && str[i] <= 'Z') {
+            cout << str[i];
+        }
+        else if (str[i] == '(') {
+            stack.push_back(str[i]);
+        }
+        else if (str[i] == '+' || str[i] == '-') {
+            while (!stack.empty() && (stack.back() == '+' || stack.back() == '-' || stack.back() == '*' || stack.back() == '/')) {
+                cout << stack.back();  stack.pop_back();
+            }
+            stack.push_back(str[i]);
+        }
+        else if (str[i] == '*' || str[i] == '/') {
+            while (!stack.empty() && (stack.back() == '*' || stack.back()=='/')) {
+                cout << stack.back();  stack.pop_back();
+            }
+            stack.push_back(str[i]);
+        }
+        else {
+            while (!stack.empty() && stack.back() != '(') {
+                cout << stack.back();  stack.pop_back();
+            }
+            if (!stack.empty() && stack.back() == '(') {
+                stack.pop_back();
+            }
+        }
+    }
 
-	while (!stack.empty()) {
-		c = stack.back();
-		stack.pop_back();
-		if (c != '(') cout << c;
-	}
-	return 0;
+    while (!stack.empty()) {
+        cout << stack.back();
+        stack.pop_back();
+    }
+    return 0;
 }
